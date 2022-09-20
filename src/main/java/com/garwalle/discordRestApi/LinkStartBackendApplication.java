b979e6e0debe88b81635fa2c17331be1339bbabe
MartinJeanne <martin.jeanne@sinay.fr>
2022-09-20 15:04:10 +0200

    Trying to import Swagger3 (don't work surprisingly)

--- src/main/java/com/garwalle/discordRestApi/LinkStartBackendApplication.java
+++ src/main/java/com/garwalle/discordRestApi/LinkStartBackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DiscordRestApiApplication {
+public class LinkStartBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiscordRestApiApplication.class, args);
+		SpringApplication.run(LinkStartBackendApplication.class, args);
 	}
 
 }
