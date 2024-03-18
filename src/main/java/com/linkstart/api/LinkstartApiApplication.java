529383d38af681153e51743a5b78ea1de4cac362
MartinJeanne <martin.jeanne@sinay.fr>
2024-03-18 17:44:42 +0100

    Rename boot class

--- src/main/java/com/linkstart/api/LinkstartApiApplication.java
+++ src/main/java/com/linkstart/api/LinkstartApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LinkstartBackendApplication {
+public class LinkstartApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LinkstartBackendApplication.class, args);
+		SpringApplication.run(LinkstartApiApplication.class, args);
 	}
 
 }
